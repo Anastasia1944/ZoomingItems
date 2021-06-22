02e897b9399eee2904fabecf4a4d6106b83af251
Anastasia <goryachevskaya.nastya@yandex.ru>
2021-06-22 16:22:17 +0300

    Image Pinch and Rotation

--- ZoomingItems/ViewController.swift
+++ ZoomingItems/ViewController.swift
@@ -27,16 +27,10 @@ class ViewController: UIViewController {
         return
       }
 
-      gestureView.center = CGPoint(
-        x: gestureView.center.x + translation.x,
-        y: gestureView.center.y + translation.y
-      )
-
+      gestureView.center = CGPoint(x: gestureView.center.x + translation.x, y: gestureView.center.y + translation.y)
       gesture.setTranslation(.zero, in: view)
         
-        guard gesture.state == .ended else {
-          return
-        }
+        guard gesture.state == .ended else { return }
 
         let velocity = gesture.velocity(in: view)
         let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
@@ -44,17 +38,27 @@ class ViewController: UIViewController {
         
         let slideFactor = 0.01 * slideMultiplier
 
-        var finalPoint = CGPoint(
-          x: gestureView.center.x + (velocity.x * slideFactor),
-          y: gestureView.center.y + (velocity.y * slideFactor)
-        )
+        var finalPoint = CGPoint(x: gestureView.center.x + (velocity.x * slideFactor), y: gestureView.center.y + (velocity.y * slideFactor))
 
         finalPoint.x = min(max(finalPoint.x, 0), view.bounds.width)
         finalPoint.y = min(max(finalPoint.y, 0), view.bounds.height)
 
         UIView.animate(withDuration: Double(slideFactor * 2), delay: 0, options: .curveEaseOut, animations: {gestureView.center = finalPoint})
-
+        
+        
     }
+    
+    @IBAction func handlePinch(_ gesture: UIPinchGestureRecognizer) {
+        guard let gestureView = gesture.view else { return }
+        gestureView.transform = gestureView.transform.scaledBy(x: gesture.scale, y: gesture.scale)
+        gesture.scale = 1
+     }
+     
+     @IBAction func handleRotate(_ gesture: UIRotationGestureRecognizer) {
+        guard let gestureView = gesture.view else { return }
+        gestureView.transform = gestureView.transform.rotated(by: gesture.rotation)
+        gesture.rotation = 0
+     }
 
 
 }
