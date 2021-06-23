536a123e7d521a5c0e50d6b3bf4adb11c50777ac
Anastasia <goryachevskaya.nastya@yandex.ru>
2021-06-23 16:48:28 +0300

    New entry point controller with table and button for transition to next screen

--- ZoomingItems/PicturesViewController.swift
+++ ZoomingItems/PicturesViewController.swift
@@ -11,10 +11,47 @@ class PicturesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+    @IBAction func handlePan(_ gesture: UIPanGestureRecognizer) {
+      let translation = gesture.translation(in: view)
+
+      guard let gestureView = gesture.view else {
+        return
+      }
+
+      gestureView.center = CGPoint(x: gestureView.center.x + translation.x, y: gestureView.center.y + translation.y)
+      gesture.setTranslation(.zero, in: view)
+        
+        guard gesture.state == .ended else { return }
+
+        let velocity = gesture.velocity(in: view)
+        let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+        let slideMultiplier = magnitude / 200
+        
+        let slideFactor = 0.01 * slideMultiplier
+
+        var finalPoint = CGPoint(x: gestureView.center.x + (velocity.x * slideFactor), y: gestureView.center.y + (velocity.y * slideFactor))
+
+        finalPoint.x = min(max(finalPoint.x, 0), view.bounds.width)
+        finalPoint.y = min(max(finalPoint.y, 0), view.bounds.height)
+
+        UIView.animate(withDuration: Double(slideFactor * 2), delay: 0, options: .curveEaseOut, animations: {gestureView.center = finalPoint})
+        
+        
+    }
+    
+    @IBAction func handlePinch(_ gesture: UIPinchGestureRecognizer) {
+        guard let gestureView = gesture.view else { return }
+        gestureView.transform = gestureView.transform.scaledBy(x: gesture.scale, y: gesture.scale)
+        gesture.scale = 1
+     }
+     
+     @IBAction func handleRotate(_ gesture: UIRotationGestureRecognizer) {
+        guard let gestureView = gesture.view else { return }
+        gestureView.transform = gestureView.transform.rotated(by: gesture.rotation)
+        gesture.rotation = 0
+     }
 
     /*
     // MARK: - Navigation
--- ZoomingItems/ViewController.swift
+++ ZoomingItems/ViewController.swift
@@ -9,57 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //@IBOutlet weak var myImageView2: UIImageView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        test_storyboard()
     }
     
-    func test_storyboard(){
-        //myImageView2.transform = myImageView2.transform.translatedBy(x: 20, y: 20)
+    @IBAction func nextScreenPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "goToPics", sender: self)
     }
     
-    @IBAction func handlePan(_ gesture: UIPanGestureRecognizer) {
-      let translation = gesture.translation(in: view)
-
-      guard let gestureView = gesture.view else {
-        return
-      }
-
-      gestureView.center = CGPoint(x: gestureView.center.x + translation.x, y: gestureView.center.y + translation.y)
-      gesture.setTranslation(.zero, in: view)
-        
-        guard gesture.state == .ended else { return }
-
-        let velocity = gesture.velocity(in: view)
-        let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
-        let slideMultiplier = magnitude / 200
-        
-        let slideFactor = 0.01 * slideMultiplier
-
-        var finalPoint = CGPoint(x: gestureView.center.x + (velocity.x * slideFactor), y: gestureView.center.y + (velocity.y * slideFactor))
-
-        finalPoint.x = min(max(finalPoint.x, 0), view.bounds.width)
-        finalPoint.y = min(max(finalPoint.y, 0), view.bounds.height)
-
-        UIView.animate(withDuration: Double(slideFactor * 2), delay: 0, options: .curveEaseOut, animations: {gestureView.center = finalPoint})
-        
-        
-    }
-    
-    @IBAction func handlePinch(_ gesture: UIPinchGestureRecognizer) {
-        guard let gestureView = gesture.view else { return }
-        gestureView.transform = gestureView.transform.scaledBy(x: gesture.scale, y: gesture.scale)
-        gesture.scale = 1
-     }
-     
-     @IBAction func handleRotate(_ gesture: UIRotationGestureRecognizer) {
-        guard let gestureView = gesture.view else { return }
-        gestureView.transform = gestureView.transform.rotated(by: gesture.rotation)
-        gesture.rotation = 0
-     }
-
 
 }
 
