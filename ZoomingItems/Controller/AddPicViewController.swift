dc5b28dd4f185a0c5a350210a94eea829ea58491
Anastasia <goryachevskaya.nastya@yandex.ru>
2021-06-29 13:55:59 +0300

    add alamofire

--- ZoomingItems/Controller/AddPicViewController.swift
+++ ZoomingItems/Controller/AddPicViewController.swift
@@ -8,27 +8,28 @@
 import UIKit
 
 class AddPicViewController: UIViewController {
-
+    
     @IBOutlet weak var picNameTextField: UITextField!
     @IBOutlet weak var picLinkTextField: UITextField!
+    @IBOutlet weak var myImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     @IBAction func addImageButtonPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
+
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
